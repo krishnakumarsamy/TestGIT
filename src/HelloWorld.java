15d60c5347f4be56f570aaf55223d7f49f589542
Krishnakumar Murugasamy <krishnakumarsamy@gmail.com>
2015-05-20 00:16:46 +0530

    7

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("TESt");
 	System.out.println("5");
-	System.out.println("6");
+	System.out.println("7");
 }
 }
