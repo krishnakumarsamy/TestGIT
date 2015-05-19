9100e71422d51014b73c0119762a28cd02aa5486
krishnakumarsamy <krishnakumarsamy@gmail.com>
2015-05-20 00:38:19 +0530

    MERGE

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,8 @@
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("TESt");
-	System.out.println("5");
+	System.out.println("8");
+	System.out.println("8");
 	System.out.println("7");
 	System.out.println("8");
 	System.out.println("9");
